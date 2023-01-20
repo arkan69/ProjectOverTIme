52a64a3ffed9f1ae764e2b4d4c7f9a66f1824152
arkan69 <59425945+arkan69@users.noreply.github.com>
2023-01-20 21:00:08 +0700

    add generalrepo and basecontroller

---
+++ API/Base/BaseController.cs
@@ -0,0 +1,106 @@
+﻿using API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Base
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController<Entity,T,X> : ControllerBase
+        where Entity : class
+        where X : IRepository<Entity,T>
+    {
+        private X _repositories;
+
+        public BaseController(X repositories)
+        {
+            _repositories = repositories;
+        }
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            try
+            {
+                var result = _repositories.Get();
+                return result.Count() == 0
+                    ? Ok(new { statusCode = 200, message = "Data Not Found!" })
+                    : Ok(result);
+            }
+            catch
+            {
+                return BadRequest(new { statusCode = 500, message = "Something Wrong!!" });
+            }
+        }
+
+        [HttpGet]
+        [Route("{key}")]
+        public ActionResult GetById(T key)
+        {
+            try
+            {
+                var result = _repositories.Get(key);
+                return result == null
+                    ? Ok(new { statusCode = 200, message = "Data Not Found!" })
+                    : Ok(new { statusCode = 200, message = "Success", data = result });
+            }
+            catch
+            {
+                return BadRequest(new { statusCode = 500, message = "Wrong Input" });
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Insert(Entity entity)
+        {
+            try
+            {
+                var result = _repositories.Insert(entity);
+                return result == 0
+                    ? Ok(new { statusCode = 200, messgae = "Data Failed to Save" })
+                    : Ok(new { statusCode = 200, messgae = "Data Has Been Saved", data = result });
+            }
+            catch
+            {
+                return BadRequest(new { statusCode = 500, message = "Input Valid Property" });
+            }
+
+        }
+
+        [HttpPut]
+        public ActionResult Update(Entity entity)
+        {
+            try
+            {
+                var result = _repositories.Update(entity);
+                return result == 0
+                    ? Ok(new { statusCode = 200, message = "Data Not Updated" })
+                    : Ok(new { statusCode = 200, message = "Data Updated", data = result });
+            }
+            catch
+            {
+                return BadRequest(new { statusCode = 500, message = "Something Wrong with your Input" });
+            }
+        }
+
+        [HttpDelete]
+        [Route("{key}")]
+        public ActionResult Delete(T key)
+        {
+            try
+            {
+                var result = _repositories.Delete(key);
+                if (result == 0)
+                {
+                    return Ok(new { statusCode = 200, message = $"Id {key} is Not Found" });
+                }
+                return Ok(new { statusCode = 200, message = "Data has been deleted", data = result });
+            }
+            catch
+            {
+                return BadRequest(new { statusCode = 500, message = "Something Wrong with your Input" });
+            }
+        }
+    }
+}
