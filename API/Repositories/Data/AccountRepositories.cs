a57b328abb01952f8e4822748c971d2dc3fb2aa7
arkan69 <59425945+arkan69@users.noreply.github.com>
2023-01-20 21:19:40 +0700

    Add repositories

---
+++ API/Repositories/Data/AccountRepositories.cs
@@ -0,0 +1,19 @@
+﻿using API.Contexts;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Repositories.Data
+{
+    public class AccountRepositories : GeneralRepository<Account, string>
+    {
+        private MyContext _context;
+        private DbSet<Account> _account;
+        private DbSet<Employee> _employees;
+        public AccountRepositories(MyContext context) : base(context)
+        {
+            _context = context;
+            _account = context.Set<Account>();
+            _employees = context.Set<Employee>();
+        }
+    }
+}
---
+++ API/Repositories/Data/EmployeeRepositories.cs
@@ -0,0 +1,12 @@
+﻿using API.Contexts;
+using API.Models;
+
+namespace API.Repositories.Data
+{
+    public class EmployeeRepositories : GeneralRepository<Employee, string>
+    {
+        public EmployeeRepositories(MyContext context) : base(context)
+        {
+        }
+    }
+}
