d445cd6a76e0e52022a75abcb234af3afa1e8dc4
thofolio <54202391+theosuryaadi@users.noreply.github.com>
2023-01-25 01:37:04 +0700

    Add Logo ORS in Login Page

--- Client/Controllers/EmployeeController.cs
+++ Client/Controllers/EmployeeController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API.Models;
+using Client.Base;
+using Client.Repositories.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Client.Controllers
 {
-    public class EmployeeController : Controller
+    public class EmployeeController : BaseController<Employee, EmployeeRepository, string>
     {
+        private readonly EmployeeRepository repository;
+        public EmployeeController(EmployeeRepository repository) : base(repository)
+        {
+            this.repository = repository;
+        }
         public IActionResult Index()
         {
             return View();
